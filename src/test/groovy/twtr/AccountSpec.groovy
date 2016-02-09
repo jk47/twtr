fd37e00fa037dea72e5708d4688beb88c0a3557a
auxxx053 <gg>
2016-02-09 09:16:48 -0600

    Added data driven unit test for account requirements #3

--- src/test/groovy/twtr/AccountSpec.groovy
+++ src/test/groovy/twtr/AccountSpec.groovy
@@ -4,6 +4,7 @@ import grails.test.mixin.TestFor
 import grails.test.mixin.TestMixin
 import grails.test.mixin.domain.DomainClassUnitTestMixin
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
@@ -30,13 +31,25 @@ class AccountSpec extends Specification {
         Account.get(account.id).realName == account.realName
     }
 
+    @Unroll('#description')
     def "invalid passwords will not be saved to db"() {
         given: "an account with invalid password"
-            def account = new Account(handle: 'coding', password:'Test1', email: 'test@gmail.com', realName: 'coding guy')
+            def account = new Account(handle: 'coding', password:inputPassword, email: 'test@gmail.com', realName: 'coding guy')
         when: "attempting to save"
             account.save()
         then: "an error will be attached to account, it will not save"
-            account.errors.errorCount > 0
+            account.errors.errorCount > 0 == expectedValidationError
+
+        where:
+        description                                             | inputPassword                 | expectedValidationError
+        'Fail on password with 7 valid characters'              | 'M'*3 + 'j'*2 + '23'          | true
+        'Pass on password with 8 valid characters'              | 'M'*3 + 'j'*3 + '23'          | false
+        'Pass on password with 9 valid characters'              | 'M'*3 + 'j'*4 + '23'          | false
+        'Pass on password with 16 valid characters'             | 'M'*7 + 'j'*7 + '23'          | false
+        'Fail on password with 17 valid characters'             | 'M'*7 + 'j'*8 + '23'          | true
+        'Fail with password that contains no numbers'           | 'M'*7 + 'j'*7                 | true
+        'Fail with password that contains no upper-case'        | 'm'*4 + 'j'*4 + '23'          | true
+        'Fail with password that contains no lower-case'        | 'M'*4 + 'J'*4 + '23'          | true
     }
 
 
