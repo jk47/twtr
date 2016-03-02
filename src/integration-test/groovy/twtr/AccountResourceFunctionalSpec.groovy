ee3fd225c471d7b83b1784aaa71e0cfef6452290
jk47 <jhnkotz@gmail.com>
2016-03-01 18:31:13 -0600

    adding tests to accountfunctionalspec

--- src/integration-test/groovy/twtr/AccountResourceFunctionalSpec.groovy
+++ src/integration-test/groovy/twtr/AccountResourceFunctionalSpec.groovy
@@ -3,8 +3,10 @@ package twtr
 
 import geb.spock.GebSpec
 import grails.test.mixin.integration.Integration
+import groovyx.net.http.HttpResponseException
 import spock.lang.Stepwise
 import groovyx.net.http.RESTClient
+import spock.lang.Unroll
 
 
 @Integration
@@ -22,9 +24,35 @@ class AccountFunctionalSpec extends GebSpec {
         def accountJSON = '{"handle": "coding", "password": "TestPass1", "email": "test@gmail.com", "realName": "coding guy"}'
 
         when: "trying to post the account via rest"
-        def resp = restClient.post(path: "/api/accounts", requestContentType: 'application/json', body: accountJSON)
+        def resp = restClient.post(path: "/api/accounts", requestContentType: "application/json", body: accountJSON)
 
         then:
         resp.status == 201
+        resp.data.id != null
+        //Account.get(resp.data.id).realName == "coding guy"
+    }
+
+    @Unroll('#description')
+    def "verify an error is returned for invalid JSON account creation request"() {
+        given: "an account with invalid JSON"
+        //def accountsBefore = Account.count()
+
+        when: "saving by submitting a JSON request"
+        def resp = restClient.post(path: "/api/accounts", requestContentType: "application/json", body: json)
+
+        then: "a 422 is received via JSON response and account is not saved to db"
+        HttpResponseException e = thrown()
+        e.cause == null
+        e.message == "Unprocessable Entity"//this is the message for a 422
+        e.statusCode == 422
+        //accountsBefore == Account.count()
+
+
+        where:
+        description                | json
+        'Fail on missing handle'   | '{"password": "TestPass1", "email": "test@gmail.com", "realName": "coding guy"}'
+        'Fail on missing email'    | '{"handle": "coding", "password": "TestPass1", "realName": "coding guy"}'
+        'Fail on missing password' | '{"handle": "coding", "email": "test@gmail.com", "realName": "coding guy"}'
+        'Fail on missing name'     | '{"handle": "coding", "password": "TestPass1", "email": "test@gmail.com"}'
     }
 }
