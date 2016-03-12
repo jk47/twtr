f693080c491b6bc7ba8b8592ed59ab996f4b06a6
auxxx053 <gg>
2016-03-12 12:35:57 -0600

    Override index method in MessageController
    MessageController will now return messages belonging to the specified
    account in the url correctly.

--- grails-app/controllers/twtr/MessageController.groovy
+++ grails-app/controllers/twtr/MessageController.groovy
@@ -1,12 +1,37 @@
 package twtr
 
+import grails.converters.JSON
 import grails.rest.RestfulController
 
-class MessageController extends RestfulController<Account>{
+class MessageController extends RestfulController<Message>{
     //static allowedMethods = [update: "PUT", show: "GET", save: "POST", delete: "DELETE"]
     static responseFormats = ['json']
 
     MessageController(){
         super(Message)
+    }
+
+    @Override
+    protected Message queryForResource(Serializable id) {
+        def accountId = params.accountId
+        Message.where {
+            id == id && account.id == accountId
+        }.find()
+    }
+
+    def index()
+    {
+        def accountId = params.accountId
+        Account account = Account.get(accountId)
+        if (account == null) {
+            render status: 404
+            return
+        }
+
+        def messages = Message.where {
+            account.id == accountId
+        }.findAll()
+
+        render(messages as JSON)
     }
 }
