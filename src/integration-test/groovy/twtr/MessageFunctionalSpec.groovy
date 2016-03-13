0d5799abca9ecd33101fe3b6ee5affccd55e4899
auxxx053 <gg>
2016-03-12 20:13:54 -0600

    More M3 updates

--- src/integration-test/groovy/twtr/MessageFunctionalSpec.groovy
+++ src/integration-test/groovy/twtr/MessageFunctionalSpec.groovy
@@ -18,7 +18,7 @@ class MessageFunctionalSpec extends GebSpec {
         restClient = new RESTClient(baseUrl)
     }
 
-    def "create a message with an id"() {
+    def "Create a message with an id"() {
         given: "an account that has been saved and a message in json form"
         def accountJSON = '{"handle": "decoding", "password": "TestPass1", "email": "uniqueemail@gmail.com", "realName": "coding guy"}'
         def accountResponse = restClient.post(path: "/api/accounts", requestContentType: "application/json", body: accountJSON)
@@ -35,11 +35,11 @@ class MessageFunctionalSpec extends GebSpec {
     }
 
     @Unroll('#description')
-    def "Return an error response from the the create message end point"() {
+    def "Return an error response from the create Message endpoint if user is not found or message text is not valid"() {
         given: "a message"
         def messageJson = '{"content": ' + messageContent + ', "account": ' + accountId + '}'
 
-        when: "creating the message via reset endpoint"
+        when: "creating the message via rest endpoint"
         def createMessageResponse = restClient.post(path: "/api/accounts/1/messages", requestContentType: "application/json", body: messageJson)
 
         then: "a 422 should be received"
@@ -47,10 +47,28 @@ class MessageFunctionalSpec extends GebSpec {
         e.statusCode == 422
 
         where:
-        description                                                      | accountId | messageContent
-        "Error when message has 41 chars"                                | '1'       | 'f' * 41
-        "Error when message has blank content"                           | '1'       | ''
-        "Error when invalid account is specified but with valid message" | '0'       | 'f' * 10
-        "Error when invalid account and invalid message are specified"   | '0'       | ''
+        description                                                                      | accountId | messageContent
+        "Return error response when message has 41 chars"                                | '1'       | 'f' * 41
+        "Return error response when message has blank content"                           | '1'       | ''
+        "Return error response when invalid account is specified but with valid message" | '0'       | 'f' * 10
+        "Return error response when invalid account and invalid message are specified"   | '0'       | ''
+    }
+
+    @Unroll('#description')
+    def "Create a REST endpoint that will return the most recent messages for an Account. The endpoint must honor a limit parameter that caps the number of responses. The default limit is 10"() {
+        given: "a message"
+        def messageJson = '{"content": "1", "account": "1"}'
+        for (int i = 1; i <10; i++) {
+            def createMessageResponse = restClient.post(path: "/api/accounts/1/messages", requestContentType: "application/json", body: messageJson)
+            assert createMessageResponse.status == 201
+            assert createMessageResponse.data.content != null
+        }
+
+        when: "get the most recent messages via rest endpoint"
+        def recentMessagesResponse = restClient.get(path: "/api/accounts/1/messages/recent", requestContentType: "application/json")
+
+        then: "200 should be received"
+        recentMessagesResponse.status == 200
+
     }
 }
