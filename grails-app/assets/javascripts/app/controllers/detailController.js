227fdf7f11148068bdb254e7614a159fc9571df3
auxxx053 <goldenascension119@gmail.com>
2016-04-14 10:51:12 -0500

    Completed U4: editing name and email

--- grails-app/assets/javascripts/app/controllers/detailController.js
+++ grails-app/assets/javascripts/app/controllers/detailController.js
@@ -3,53 +3,53 @@ app.controller('detailController', function ($scope, $location, $http, securityS
     $scope.auth.token = securityService.getToken();
     $scope.username = securityService.getUsername();
     $scope.currentUser = securityService.getCurrentUser();
-    $http.get('/api/accounts/handle='+$scope.currentUser.username, {headers: {'X-Auth-Token': $scope.auth.token.toString()}})
-        .success(function (data){
+    $http.get('/api/accounts/handle=' + $scope.currentUser.username, {headers: {'X-Auth-Token': $scope.auth.token.toString()}})
+        .success(function (data) {
             $scope.currentId = data.id;
         })
 
     // get the handle that the page should show
     var params = $location.search();
-    if(!params['handle']){
+    if (!params['handle']) {
         $scope.handle = $scope.username;
     }
     else {
         $scope.handle = params['handle'];
     }
 
-    $scope.getAccount = function() {
-        $http.get('/api/accounts/handle=john' ,{headers: {'X-Auth-Token': $scope.auth.token.toString()}})
-            .success(function(data){
+    $scope.getAccount = function () {
+        $http.get('/api/accounts/handle=john', {headers: {'X-Auth-Token': $scope.auth.token.toString()}})
+            .success(function (data) {
                 $scope.account = data;
                 $scope.getTweets();
                 $scope.isCurrentUser();
                 $scope.isFollower();
             })
-            .error(function (error){
+            .error(function (error) {
                 alert(error.toString());
             })
 
     };
 
-    $scope.getTweets = function() {
+    $scope.getTweets = function () {
         $http.get('/api/accounts/' + $scope.account.id + '/messages', {headers: {'X-Auth-Token': $scope.auth.token.toString()}})
-            .success(function(data){
+            .success(function (data) {
                 $scope.tweets = data;
             })
-            .error(function (error){
+            .error(function (error) {
                 alert("get tweets error");
             })
-            .finally( function(){
+            .finally(function () {
                 // not sure if i need this
             })
     };
 
-    $scope.isFollower = function() {
+    $scope.isFollower = function () {
         $http.get('/api/accounts/' + $scope.account.id + '/followers', {headers: {'X-Auth-Token': $scope.auth.token.toString()}})
-            .success(function (data){
+            .success(function (data) {
                 var isAFollower = false;
-                for (var i = 0; i<data.length; i++){
-                    if (data[i].handle == $scope.handle){
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i].handle == $scope.handle) {
 
                         isAFollower = true;
                         break;
@@ -57,36 +57,50 @@ app.controller('detailController', function ($scope, $location, $http, securityS
                 }
                 $scope.isAFollower = isAFollower;
             })
-            .error( function(error){
+            .error(function (error) {
                 alert(error);
             })
 
     };
 
-    $scope.isCurrentUser = function() {
+    $scope.isCurrentUser = function () {
         $scope.myOwnDetails = ($scope.username == $scope.account.handle);
     };
 
-    $scope.startFollowing = function() {
+    $scope.startFollowing = function () {
         $http.get('/api/accounts/' + $scope.currentId + '/follow/' + $scope.account.id, {headers: {'X-Auth-Token': $scope.auth.token.toString()}})
-            .success(function (data){
+            .success(function (data) {
                 $scope.getAccount();//refresh the current account
             })
-            .error(function (error){
+            .error(function (error) {
                 alert(error);
             })
     };
 
-    $scope.updateUserDetails = function() {
-        var updates = new Object();
-        updates.handle = $scope.newHandle;
-        updates.newEmailAddress = $scope.newEmail;
-        var jsonbody = JSON.stringify(updates);
-        //TO DO: make new endpoint for editing the user details
-        
-        
+    $scope.updateUserDetails = function () {
+        delete $scope.error
+        delete $scope.success
+
+        var newUserDetails = new Object();
+        newUserDetails.email = $scope.account.email
+        newUserDetails.realName = $scope.account.realName
+        var jsonBody = JSON.stringify(newUserDetails)
+
+        $http.put('/api/accounts/' + $scope.currentId + '/', jsonBody,
+            {
+                headers: {
+                    'X-Auth-Token': $scope.auth.token.toString(),
+                    'Content-Type': 'application/json'
+                }
+            })
+            .success(function (data, status, headers) {
+                $scope.success = 'Update successful.'
+            })
+            .error(function (error) {
+                $scope.error = error;
+            })
+
     };
 
 
-    
 });
