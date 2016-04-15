0961baf11ce6a949fc01cda51a8b8f72d2939ee8
jk47 <jhnkotz@gmail.com>
2016-04-14 21:01:40 -0500

    added four tests

--- src/integration-test/groovy/twtr/AngularFunctionalSpec.groovy
+++ src/integration-test/groovy/twtr/AngularFunctionalSpec.groovy
@@ -2,18 +2,74 @@ package twtr
 
 import geb.spock.GebSpec
 import grails.test.mixin.integration.Integration
+import spock.lang.Stepwise
 
 @Integration
+@Stepwise
 class AngularFunctionalSpec extends GebSpec {
 
-    def 'twtr login page displays'() {
-        when:
-        go("/")
+    def signIn(){
+        go("localhost:8080")
+        waitFor {
+            $('#usernameLoginBox').isDisplayed()
+        }
+        $('#usernameLoginBox').value("john")
+        $('#passwordLoginBox').value("Password1")
+        $('#loginButton').click()
+    }
 
-        then: 'Static welcome displayed properly'
-        $('h1').first().text() == 'Welcome to the sample Grails 3 Angular App'
+    def 'L1/L2: route to login page when not logged in'() {
+        when: 'not logged in and going to non login page'
+        go("#/detail")
 
-        and: 'Angular generated test displayed properly'
-        $('h2').first().text() == 'Hello Stranger'
+        then: 'login dialog displayed'
+        $('#usernameLoginBox').isDisplayed()
+        $('#passwordLoginBox').isDisplayed()
+    }
+
+    def 'L3: error displayed for invalid login'() {
+        when: 'typing wrong password'
+        go('localhost:8080')
+        waitFor {
+            $('#usernameLoginBox').isDisplayed()
+        }
+        $('#usernameLoginBox').value("john")
+        $('#passwordLoginBox').value("bogusPassword")
+        $('#loginButton').click()
+
+
+        then: 'error is shown to the user'
+        waitFor {
+            $('#errorMessage').isDisplayed()
+        }
+
+    }
+
+    def 'S1: search box allows user to search messages'() {
+        when: 'logged in'
+        signIn()
+
+        then: 'search box is displayed'
+        waitFor {
+            $('#searchBox').isDisplayed()
+        }
+
+    }
+
+    def 'S2: messages are displayed in scrollable area'() {
+        given: 'logged in'
+        signIn()
+
+        when: 'searching'
+        Thread.sleep(2000)
+        $('#searchBox').value('#')
+        $('#searchButton').click()
+
+        then: 'results in scrollable list'
+        waitFor {
+            $('#resultsDiv').isDisplayed()
+        }
+        $('#resultsDiv').height < $('#searchResultsTable').height
+
     }
 }
