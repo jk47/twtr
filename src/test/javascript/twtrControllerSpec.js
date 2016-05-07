ac494fcd0dc4add44b4d816a322c05fb783debd7
jk47 <jhnkotz@gmail.com>
2016-05-07 11:14:51 -0500

    jasmine passing

--- src/test/javascript/twtrControllerSpec.js
+++ src/test/javascript/twtrControllerSpec.js
@@ -1,59 +1,16 @@
-
 describe('twtrController', function () {
     beforeEach(module('app'));
 
-    var $controller;
-    var $rootScope;
-    var $scope;
-
-    var currentUser = undefined;
-
-    var securityService = {
-        currentUser: function () {
-            return currentUser;
-        },
-
-        logout: function () {
-        }
-    };
-
-    beforeEach(inject(function (_$controller_, _$rootScope_) {
-        // The injector unwraps the underscores (_) from around the parameter names when matching
-        $controller = _$controller_;
-        $rootScope = _$rootScope_;
-        $scope = {};
-    }));
-
-    describe("true", function()
-    {
-        it("Should be true", function(){
-            expect(true).toBeTruthy()
-
-        });
-
-    });
-
-});
-
-
-
-
-//-----------------------------BELOW CODE WILL BREAK THE TESTS --------------------------------//
-
-
-/*
-describe('twtrController', function () {
-    beforeEach(module('app'));
-
-    var $scope;
-    var $controller;
     var $httpBackend;
+    var $scope;
+    var $controller;
     var securityService;
     var tweetService;
 
 
-    beforeEach(inject(function (_$controller_, _$httpBackend_, _$rootScope_, _securityService_, _tweetService_ ) {
+    beforeEach(inject(function (_$httpBackend_, _$controller_ , _$rootScope_, _securityService_, _tweetService_) {
         // The injector unwraps the underscores (_) from around the parameter names when matching
+        $httpBackend = _$httpBackend_;
         securityService = _securityService_;
         securityService.setCurrentUser({
             username: 'john',
@@ -65,25 +22,54 @@ describe('twtrController', function () {
         tweetService = _tweetService_;
         $scope = _$rootScope_.$new();
         $controller = _$controller_;
-        $httpBackend = _$httpBackend_;
+
     }));
 
 
+    describe("tweeting a message", function () {
 
-
-    describe("tweeting a message", function()
-    {
-
-        beforeEach(function ()
-        {
-            $controller('twtrController', {$scope: $scope, securityService: securityService, tweetService: tweetService});
+        beforeEach(function () {
             spyOn(tweetService, 'doTweet');
+            $controller('twtrController', {
+                $scope: $scope,
+                securityService: securityService,
+                tweetService: tweetService
+            });
+
         });
 
-        it("handles the posting of a message", function(){
-            $httpBackend.expectGET('/api/accounts/handle=john').respond(200);
-            $httpBackend.expectPOST('/api/accounts/2/messages', {content: "twitter message content", account:2}).respond(201);
-            
+        it("handles the posting of a message", function () {
+            $httpBackend.expectGET('/api/accounts/handle=john').respond(200,
+                {
+                    dateCreated: "2016-05-07T14:49:37Z",
+                    email: "john@gmail.com",
+                    followerCount: "1",
+                    followingCount: "0",
+                    handle: "john",
+                    id: 2,
+                    lastUpdated: "2016-05-07T14:49:37Z",
+                    password: "thePassword!",
+                    realName: "john"
+                });
+
+            $httpBackend.expectGET('/api/accounts/2/messages').respond(200, [{}]);
+
+            /*$httpBackend.expectPOST('/api/accounts/2/messages', {
+                content: "twitter message content",
+                account: 2
+            }).respond(201,
+                {
+                    class: "twtr.Message",
+                    id: 109,
+                    account: {
+                        class: "twtr.Account",
+                        id: 1
+                    },
+                    content: "dsf",
+                    dateCreated: "2016-05-07T15:19:11Z"
+                });
+        */
+
             expect($scope).toBeDefined();
             expect($scope.currentUser).toBeDefined();
 
@@ -95,4 +81,3 @@ describe('twtrController', function () {
     });
 });
 
- */
